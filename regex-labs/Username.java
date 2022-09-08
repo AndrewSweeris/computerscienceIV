75f34bfb3093fecbca69c98934d11ea7be1f8c62
Andrew M. Sweeris <46544596+AndrewSweeris@users.noreply.github.com>
2022-09-08 08:17:59 -0500

    Add files via upload
    Uploaded final commits to regex labs. Finished Substitution, Username Validation, and Document Analysis

--- regex-labs/Username.java
+++ regex-labs/Username.java
@@ -8,21 +8,26 @@ import java.util.regex.Pattern;
 
 public class Username
 {
+	private static Pattern p = Pattern.compile("[[a-z][A-Z][0-9][-][_]]{4,16}");
+	private static Matcher m;
 	private int count;
 
 	public Username()
 	{
-
+		count = 0;
 	}
 
-
-
-
-
-
+	public boolean isValid(String username) {
+		m = p.matcher(username);
+		if (m.matches()) {
+			count++;
+			return true;
+		}
+		return false;
+	}
 
 	public String toString()
 	{
-		return "";
+		return "The list has " + count + " valid user names.";
 	}
 }
--- regex-labs/UsernameRunner.java
+++ regex-labs/UsernameRunner.java
@@ -16,8 +16,13 @@ public class UsernameRunner
 		Scanner scan = new Scanner(new File("names.txt"));
 		String line;
 		List<String> usernameList = new ArrayList<String>();
+		Username username = new Username();
 		while (scan.hasNextLine()) {
 			line = scan.nextLine();
+			if (username.isValid(line)) {
+				usernameList.add(line);
+			}
 		}
+		out.println(username.toString());
 	}
 }
